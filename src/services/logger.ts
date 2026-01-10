62e47f103a29e7977c03fca93ed4253afd143df3
2a4073 <sbtk666+buin@gmail.com>
2026-01-10 15:57:41 +0900

    feat: コンソールへのロガーを追加

---
+++ src/services/logger.ts
@@ -0,0 +1,37 @@
+import { Logger, LogLevels } from "./logger.interface";
+
+export class LogConsole implements Logger {
+    private readonly timeOptions: Intl.DateTimeFormatOptions = {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+    };
+
+    write (message: string, level: LogLevels): void {
+        const formatedMassage = this.formatMessage(message, level);
+
+        switch (level) {
+            case "WARN":
+                console.warn(formatedMassage);
+                break;
+            case "ERROR":
+                console.error(formatedMassage);
+                break;
+            default:
+                console.log(formatedMassage);
+                break;
+        }
+    }
+
+    private formatMessage(message: string, level: LogLevels): string {
+        const timestamp = this.formatTimestamp();
+        return `[${timestamp}] [${level.toString()}] ${message}`;
+    }
+
+    private formatTimestamp(): string {
+        return Intl.DateTimeFormat("ja-JP", this.timeOptions).format(new Date).toString();
+    }
+}
